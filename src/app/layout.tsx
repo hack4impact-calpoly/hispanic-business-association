6fd34c82ecb5ae1f11622a300583caf95b1b9196
Belal Elshenety <belal.elshenety@gmail.com>
2025-01-21 19:06:06 -0800

    updated clerk to be only on client side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import "@/styles/global.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
