38afc5536fae94b3b7e54529edb7ba96615c9ebe
Belal Elshenety <belal.elshenety@gmail.com>
2025-01-22 00:30:11 -0800

    added logo and movel layout files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import "@/styles/global.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
