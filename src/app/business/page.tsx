67988c5f94641585bae829e7e634ae1875b4db39
Belal Elshenety <belal.elshenety@gmail.com>
2025-01-28 02:21:35 -0800

    business page now using business card and contact info card

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -1,3 +1,11 @@
+import ContactInfoCard from "@/components/ui/ContactInfoCard";
+import BusinessInfoCard from "@/components/ui/BusinessInfoCard";
+
 export default function Page() {
-  return <div>This is a business page using App Router!</div>;
+  return (
+    <main className="flex justify-center items-end min-h-screen bg-gray-100 gap-x-28 pb-20">
+      <BusinessInfoCard />
+      <ContactInfoCard />
+    </main>
+  );
 }
