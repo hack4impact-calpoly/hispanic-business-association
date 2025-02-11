9c833159236815c2526e699e70ba085e5f01a226
Sean Griffin <135201895+Shonueld@users.noreply.github.com>
2025-02-11 15:01:51 -0800

    5 sidebar component (#32)
    * created new branch
    
    * feat: sidebar component added, need to fix bug where it only shows for split second
    
    * stable
    
    * feat: added base sidebar on /sidebar directory
    
    * feat: stylized sidebar
    
    * feat: added logo image and fixed overlay bug
    
    ---------
    
    Co-authored-by: Sean Griffin <seangriffin@Seans-MacBook-Pro.local>

---
+++ src/app/sidebar/page.tsx
@@ -0,0 +1,10 @@
+import DesktopSidebar from "@/components/ui/DesktopSidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <DesktopSidebar />
+    </SidebarProvider>
+  );
+}
