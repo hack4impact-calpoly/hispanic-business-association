f4e7edbf7747ffd3ad67215c8c22df3557752150
lamkin1 <jamesclamkin@gmail.com>
2025-02-11 17:11:14 -0800

    Revert "fix: removed Links from NavigationMenuLink to fix error"
    This reverts commit 05f2f71f04cbe17cf3f5f97a0fd1b699168df5ce.

--- src/components/layout/MobileBottomNav.tsx
+++ src/components/layout/MobileBottomNav.tsx
@@ -41,14 +41,34 @@ export default function Navbar() {
     <div className="fixed inset-x-0 bottom-0 z-50 bg-[#D9D9D9] shadow-lg h-[92px]">
       <NavigationMenu className="w-full !max-w-full flex mt-[11px]">
         <NavigationMenuList className="w-full flex justify-center items-center space-x-[45px] ">
-          {userRole &&
-            menuItems[userRole]?.map((item, index) => (
-              <NavigationMenuItem key={index} className="flex justify-center">
-                <NavigationMenuLink href={item.href} className="flex flex-col items-center">
-                  <Image src={item.src} alt={item.alt} width={item.width} height={item.height} />
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            ))}
+          <NavigationMenuItem className="flex justify-center">
+            <NavigationMenuLink className="flex flex-col items-center">
+              <Link href="/admin">
+                <Image src="/icons/Phone - Dashboard.png" alt="Dashboard" width={41} height={43} />
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="flex justify-center">
+            <NavigationMenuLink className="flex flex-col items-center">
+              <Link href="/admin/analytics">
+                <Image src="/icons/Phone - Analytics.png" alt="Analytics" width={40} height={43} />
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="flex justify-center">
+            <NavigationMenuLink className="flex flex-col items-center">
+              <Link href="/admin/requests">
+                <Image src="/icons/Phone - Requests.png" alt="Requests" width={40} height={43} />
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="flex justify-center">
+            <NavigationMenuLink className="flex flex-col items-center">
+              <Link href="/admin/automations">
+                <Image src="/icons/Phone - Automated.png" alt="Automated" width={50} height={43} />
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
     </div>
