11db4050b3c891eb0f8665bfbfa5c6b101b43335
Kevin Rutledge <kevin.rutledge.89@gmail.com>
2025-02-17 18:35:22 -0800

    refractor(business): update background to white

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -7,7 +7,7 @@ import BusinessInfoCard from "@/components/ui/BusinessInfoCard";
 export default function Page() {
   return (
     <ResponsiveLayout title="Dashboard">
-      <div className="flex flex-col md:flex-row md:justify-center md:items-end min-h-[calc(100vh-200px)] bg-gray-100 gap-6 md:gap-x-28 p-4 md:pb-20">
+      <div className="flex flex-col md:flex-row md:justify-center md:items-end min-h-[calc(100vh-200px)] bg-white gap-6 md:gap-x-28 p-4 md:pb-20">
         <BusinessInfoCard />
         <ContactInfoCard />
       </div>
