240149220cd29cb6df8a8278d129360fbca25be5
Belal Elshenety <belal.elshenety@gmail.com>
2025-03-20 09:25:14 -0700

    fixed routing to signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ export default function Login() {
           </form>
 
           <div className="mt-4 text-center text-sm">
-            <a href="/signup" className="text-blue-600 hover:underline">
+            <a href="/business/signUp" className="text-blue-600 hover:underline">
               Sign up
             </a>{" "}
             |
