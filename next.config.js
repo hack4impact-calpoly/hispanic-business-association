55187e5e36143be28825ef4d8d9aaca5a9667e80
Kevin Rutledge <kevin.rutledge.89@gmail.com>
2025-04-14 00:14:25 -0700

    feat: added config to allow image loading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["hba-website-test-images.s3.us-west-2.amazonaws.com"],
+  },
+};
 
 module.exports = nextConfig;
