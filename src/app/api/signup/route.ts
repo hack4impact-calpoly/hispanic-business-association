a8630f964f23e693d4f93527a01296697cc633d4
Belal Elshenety <belal.elshenety@gmail.com>
2025-05-14 17:33:50 -0700

    sign up connected to mongo

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -5,18 +5,10 @@ import { currentUser } from "@clerk/nextjs/server";
 
 export async function GET(req: NextRequest) {
   try {
-    // Check authentication
-    // const user = await currentUser();
-    // if (!user) {
-    //   return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    // }
-    console.log("got here");
     await connectDB();
 
     // get all requests from mongo
     const requestData = await SignupRequest.find({});
-    console.log(requestData);
-    console.log(Array.isArray(requestData));
     return NextResponse.json(requestData);
   } catch (error) {
     console.error("Error fetching request:", error);
