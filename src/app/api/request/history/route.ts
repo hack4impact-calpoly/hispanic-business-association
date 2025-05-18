cccf49303d53a5ef12bcafd7af6201fa72e2d025
Belal Elshenety <belal.elshenety@gmail.com>
2025-05-18 00:55:34 -0700

    suppressing dynamic headeers warning since we need to send period in history route header

--- src/app/api/request/history/route.ts
+++ src/app/api/request/history/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import RequestHistory from "@/database/requestHistorySchema";
