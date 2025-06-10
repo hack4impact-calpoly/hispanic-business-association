34f730b592c6b0bc6688d627cfee2f7395b3d206
Belal Elshenety <belal.elshenety@gmail.com>
2025-06-09 17:55:01 -0700

    updating payment directly through webhook

--- src/app/api/square/webhooks/route.ts
+++ src/app/api/square/webhooks/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db"; // Assuming you have a connectDB function
 import { WebhooksHelper } from "square";
+import Business from "@/database/businessSchema"; // Assuming you have a Business schema
 
 export const runtime = "nodejs";
 
@@ -40,22 +41,31 @@ export async function POST(req: NextRequest) {
     // Handle the payment.updated event
     if (event.type === "payment.updated") {
       const payment = event.data.object;
-
+      console.log("Payment updated event received:", payment);
       // Check if the payment status is 'COMPLETED'
       if (payment.status === "COMPLETED") {
         const paymentId = payment.id;
         const orderId = payment.order_id;
         const amount = payment.amount_money.amount; // Amount in the smallest unit (e.g., cents)
         const currency = payment.amount_money.currency;
+        const clerkUserID = payment.note;
 
         console.log("Payment completed:", { paymentId, orderId, amount, currency });
 
+        const oneYearFromNow = new Date();
+        oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
+        const business = await Business.updateOne(
+          { clerkUserID: clerkUserID },
+          { $set: { membershipExpiryDate: oneYearFromNow, lastPayDate: new Date() } },
+          { upsert: true },
+        );
         return NextResponse.json({ message: "Payment processed successfully" }, { status: 200 });
       } else {
+        console.log("Payment not completed:", payment.status);
         return NextResponse.json({ message: "Payment not completed" }, { status: 200 });
       }
     }
-
+    console.log("Event type not handled:", event.type);
     // Return a 400 if the event type is not what we expect
     return NextResponse.json({ message: "Invalid event type" }, { status: 400 });
   } catch (error) {
