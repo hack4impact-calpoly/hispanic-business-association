db0fe159c23069e750048da37ca3d0068372d91d
Belal Elshenety <belal.elshenety@gmail.com>
2025-06-13 21:10:52 -0700

    added css

--- src/components/ui/GeneralAdminComponents/EditPayment.tsx
+++ src/components/ui/GeneralAdminComponents/EditPayment.tsx
@@ -7,7 +7,7 @@ import { useTranslations, useLocale } from "next-intl";
 import { DayPicker } from "react-day-picker";
 import { useBusinessById } from "@/hooks/swrHooks";
 import { mutate } from "swr";
-import "react-day-picker/dist/style.css";
+import "@/styles/react-day-picker.css";
 import { enUS, es } from "react-day-picker/locale";
 
 interface EditPaymentProps {
